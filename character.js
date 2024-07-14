7653277b46af742af4149e7c49e1390effd69d86
cxmluwu <camilosenpaii@gmail.com>
2024-07-14 18:52:41 -0500

    Add files via upload

---
+++ character.js
@@ -0,0 +1,237 @@
+function cargar(opcion){
+    var nombre = document.getElementById("nombre");
+    var descripcion = document.getElementById("descripcion");
+    var fotoPrincipal = document.getElementById("personaje-uwu");
+    var raresa = document.getElementById("raresa");
+    var arma = document.getElementById("arma");
+    var elementos = document.getElementById("elementos");
+    var personaje = document.getElementById("personaje-uwu")
+    var nikke = document.getElementById("nikke")
+    var bg = document.getElementById("main-2")
+
+    
+      
+
+
+
+    
+
+
+
+    if(opcion==1){
+        nombre.innerHTML = " Power "
+        descripcion.innerHTML = "Miembro de la División Especial 4 de Exterminio del Diablo de Seguridad Pública Demonio del diablo de sangre.";
+        fotoPrincipal.src = "img/pj/power_FB.webp";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/cohetes.png";
+        elementos.src = "img/elementos/fire.png";
+        nikke.src = "img/nikkes/1.webp";
+        bg.style.backgroundImage = "url('img/swiper/Power.jfif')";
+        
+        
+        
+        
+    }
+    if(opcion==2){
+        nombre.innerHTML = " Rosanna "
+        descripcion.innerHTML = "Rosanna era la líder de una organización criminal llamada Hedonia. A pesar de sobrevivir a un atentado contra su vida, pierde todo lo que tenía y su gente fue gravemente herida. Ahora busca al culpable para cobrar venganza.";
+        fotoPrincipal.src = "img/pj/admin.png" 
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/mini.png";
+        elementos.src = "img/elementos/Electric.png";
+        nikke.src = "img/nikkes/5.webp";
+        bg.style.backgroundImage = "url(img/swiper/rosanne.jpg)";
+    }
+    if(opcion==3){
+        nombre.innerHTML = " Blanc "
+        descripcion.innerHTML = "Un miembro del escuadrón 777 que atrae a una multitud de clientes a Coin Rush. Nada le produce mayor placer que disfrazarse de conejita, y se enorgullece enormemente de sus espectáculos que agradan al público. A diferencia de su tímida hermana, Blanc es una exuberante extrovertida.";
+        fotoPrincipal.src = "img/pj/blanc_full.png";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/Rifle.png"
+        elementos.src = "img/elementos/Wind.png";
+        nikke.src = "img/nikkes/5.webp";
+        bg.style.backgroundImage = "url('img/swiper/blanc.jpg')";
+    }
+    if(opcion==4){
+        nombre.innerHTML = " Noir "
+        descripcion.innerHTML = "Un miembro del escuadrón 777 que atrae a una multitud de clientes a Coin Rush. La hermana mayor biológica de Light Bunny Blanc, Una chica pusilánime que siente cariño y orgullosa de su hermana decididamente más atrevida.";
+        fotoPrincipal.src = "img/pj/noir_FB.webp";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/escopeta.png";
+        elementos.src = "img/elementos/wind.png";
+        nikke.src = "img/nikkes/5.webp";
+        bg.style.backgroundImage = "url('img/swiper/noir.jfif')";
+    }
+    if(opcion==5){
+        nombre.innerHTML = " Makima "
+        descripcion.innerHTML = "Miembro de la División Especial 4 de Exterminio de Demonios de Seguridad Pública. Un cazador de demonios que trabaja directamente bajo las órdenes del Secretario Jefe del Gabinete.";
+        fotoPrincipal.src = "img/pj/makima.webp";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/mp5.png";
+        elementos.src = "img/elementos/Water.png";
+        nikke.src = "img/nikkes/1.webp";
+        bg.style.backgroundImage = "url('img/swiper/makima.jpg')";
+    }
+    if(opcion==6){
+        nombre.innerHTML = " Neon: Blue Ocean "
+        descripcion.innerHTML = "Una fanática de las armas que no piensa en nada más. Ella es de Counters, pero su verdadera identidad como espía es un secreto a voces.";
+        fotoPrincipal.src = "img/pj/mary.png";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/mp5.png";
+        elementos.src = "img/elementos/Water.png";
+        nikke.src = "img/nikkes/2.webp";
+        bg.style.backgroundImage = "url('img/swiper/neon.jpg')";
+    }
+    if(opcion==7){
+        nombre.innerHTML = " A2 "
+        descripcion.innerHTML = "Un vórtice lleno de odio con un pasado doloroso. YoRHa No.2 Android tipo A.";
+        fotoPrincipal.src = "img/pj/centi.png";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/cohetes.png";
+        elementos.src = "img/elementos/Fire.png";
+        nikke.src = "img/nikkes/1.webp";
+        bg.style.backgroundImage = "url('img/swiper/a2.jpg')";
+    }
+
+    if(opcion==8){
+        nombre.innerHTML = " Mast "
+        descripcion.innerHTML = "Mast ha deseado llegar a alta mar desde que vio las aventuras de capa y espada retratadas en las películas de piratas cuando era niña.";
+        fotoPrincipal.src = "img/pj/noise.png";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/mp5.png";
+        elementos.src = "img/elementos/Electric.png ";
+        nikke.src = "img/nikkes/2.webp";
+        bg.style.backgroundImage = "url('img/swiper/must.jpg')";
+    }
+    if(opcion==9){
+        nombre.innerHTML = " Anis: Summer "
+        descripcion.innerHTML = "Una chica que sabe exactamente cómo sobrevivir en este mundo desolado. A medio camino entre la sombría Rapi y el extravagante Neon, le gusta contar chistes para aligerar el ambiente cuando las cosas se ponen demasiado tensas.";
+        fotoPrincipal.src = "img/pj/anis.png";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/escopeta.png";
+        elementos.src = "img/elementos/Electric.png";
+        nikke.src = "img/nikkes/5.webp";
+        bg.style.backgroundImage = "url('img/swiper/anis.jpg')";
+    }
+    if(opcion==10){
+        nombre.innerHTML = " Viper "
+        descripcion.innerHTML = "Oficial de Estado Mayor del Escuadrón Exótico. Puede manipular a cualquier humano con su engaño verbal y su belleza física.";
+        fotoPrincipal.src = "img/pj/viper.png";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/escopeta.png";
+        elementos.src = "img/elementos/Water.png";
+        nikke.src = "img/nikkes/5.webp";
+        bg.style.backgroundImage = "url('img/swiper/viper.jpg')";
+    }
+    if(opcion==11){
+        nombre.innerHTML = " Mary "
+        descripcion.innerHTML = "Un ángel de blanco que pasa todas sus vacaciones curando a la gente. Detrás de su amable y gentil sonrisa, se esconden secretos que nadie conoce.";
+        fotoPrincipal.src = "img/pj/firma.png";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/sniper.png";
+        elementos.src = "img/elementos/Water.png";
+        nikke.src = "img/nikkes/5.webp";
+        bg.style.backgroundImage = "url('img/swiper/mary.jpg')";
+    }
+    if(opcion==12){
+        nombre.innerHTML = " Dorothy "
+        descripcion.innerHTML = "En un equipo conocido por su fuerza superlativa, ella es reconocida como la más poderosa entre todos ellos. Su sonrisa angelical oculta el verdadero contenido de su corazón, que sigue siendo un misterio.";
+        fotoPrincipal.src = "img/pj/folkwang.png";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/Rifle.png";
+        elementos.src = "img/elementos/Water.png";
+        nikke.src = "img/nikkes/4.webp";
+        bg.style.backgroundImage = "url('img/swiper/doro.jfif')";
+    }
+    if(opcion==13){
+        nombre.innerHTML = " YoRHa No.2 Type B "
+        descripcion.innerHTML = "Una espada taciturna de resolución silenciosa. Androide YoRHa No.2 tipo B.";
+        fotoPrincipal.src = "img/pj/yulha.png";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/Rifle.png";
+        elementos.src = "img/elementos/Fire.png";
+        nikke.src = "img/nikkes/1.webp";
+        bg.style.backgroundImage = "url('img/swiper/nier.jpg')";
+    }
+    if(opcion==14){
+        nombre.innerHTML = " Helm "
+        descripcion.innerHTML = "El líder de Aegis y el capitán del Admire, un buque de guerra anfibio. Espera ansiosamente el día en que pueda demostrar su valía en el combate naval.";
+        fotoPrincipal.src = "img/pj/helmet.png";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/sniper.png";
+        elementos.src = "img/elementos/Water.png";
+        nikke.src = "img/nikkes/2.webp";
+        bg.style.backgroundImage = "url('img/swiper/helm.jfif')";
+    }
+    if(opcion==15){
+        nombre.innerHTML = " Jackal "
+        descripcion.innerHTML = "Un subordinado Nikke del escuadrón Exotic. El leal lacayo de Crow. Totalmente preparado para cumplir las órdenes de Crow en cualquier momento dado.";
+        fotoPrincipal.src = "img/pj/jackal.png";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/cohetes.png";
+        elementos.src = "img/elementos/Iron.png";
+        nikke.src = "img/nikkes/3.webp";
+        bg.style.backgroundImage = "url('img/swiper/jackal.jfif')";
+    }
+    if(opcion==16){
+        nombre.innerHTML = " D "
+        descripcion.innerHTML = "El líder de Perilous Siege. Ni una sola alma malvada escapará de su alcance, mientras ella crea que su muerte será beneficiosa para el mundo. Tiene tendencia a exagerar su personalidad cuando lleva a cabo una misión.";
+        fotoPrincipal.src = "img/pj/maiden1.png";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/mp5.png";
+        elementos.src = "img/elementos/Wind.png";
+        nikke.src = "img/nikkes/2.webp";
+        bg.style.backgroundImage = "url('img/swiper/d.jfif')";
+    }
+    if(opcion==17){
+        nombre.innerHTML = " Neve "
+        descripcion.innerHTML = "Una subordinada Nikke del Unlimited Squad, siempre está buscando osos polares. Tiene tendencia a hibernar, ya que siente que imitar el comportamiento de los osos polares es la clave para encontrarlos.";
+        fotoPrincipal.src = "img/pj/neve1.png";
+        raresa.src = "img/raresa/SR.png";
+        arma.src = "img/arma/escopeta.png";
+        elementos.src = "img/elementos/Water.png";
+        nikke.src = "img/nikkes/5.webp";
+        bg.style.backgroundImage = "url('img/swiper/neve.jfif')";
+    }
+    if(opcion==18){
+        nombre.innerHTML = " Quency "
+        descripcion.innerHTML = "Una maestra del escapismo que puede escapar de cualquier prisión donde esté cautiva. Un individuo muy amable que sobresale en el manejo y trato. Ella siempre es el alma de la fiesta en el Centro de Rehabilitación.";
+        fotoPrincipal.src = "img/pj/quency.png";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/mp5.png";
+        elementos.src = "img/elementos/Electric.png";
+        nikke.src = "img/nikkes/3.webp";
+        bg.style.backgroundImage = "url('img/swiper/quency.jpg')";
+    }
+    if(opcion==19){
+        nombre.innerHTML = " Sakura "
+        descripcion.innerHTML = "Una Nikke que pertenece a Underworld Queen y que también es la líder de un grupo yakuza conocido como Seimeikai.";
+        fotoPrincipal.src = "img/pj/scarlet.png";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/sniper.png";
+        elementos.src = "img/elementos/Fire.png";
+        nikke.src = "img/nikkes/5.webp";
+        bg.style.backgroundImage = "url('img/swiper/sakura.jfif')";
+    }
+    if(opcion==20){
+        nombre.innerHTML = " Soda  "
+        descripcion.innerHTML = "El miembro de Maid For You responsable principalmente de las tareas domésticas. Casi siempre se la puede ver con una sonrisa permanente en su rostro, que nunca se desvanece incluso cuando comete algún que otro error en el trabajo. Esta alegría infatigable es contagiosa para quienes frecuentan Soda.";
+        fotoPrincipal.src = "img/pj/soda.png";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/mini.png";
+        elementos.src = "img/elementos/Fire.png";
+        nikke.src = "img/nikkes/5.webp";
+        bg.style.backgroundImage = "url('img/swiper/soda.jpg')";
+    }
+    if(opcion==21){
+        nombre.innerHTML = " Guilty  "
+        descripcion.innerHTML = "Un subordinado Nikke del Real Kindness Squad. Posee una fuerza increíble.";
+        fotoPrincipal.src = "img/pj/volume.png";
+        raresa.src = "img/raresa/SSR.png";
+        arma.src = "img/arma/escopeta.png";
+        elementos.src = "img/elementos/Wind.png";
+        nikke.src = "img/nikkes/3.webp";
+        bg.style.backgroundImage = "url('img/swiper/Guilty.jfif')";
+    }
+    
+}
